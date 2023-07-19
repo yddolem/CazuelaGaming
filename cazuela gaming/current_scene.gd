65413a67f02aeedc3596e2ee0e03c0c9dbf95958
Zelcris <xzelcris@gmail.com>
2023-07-19 02:14:00 -0400

    Update current_scene.gd
    Niveles añadidos al arreglo

--- cazuela gaming/current_scene.gd
+++ cazuela gaming/current_scene.gd
@@ -2,7 +2,9 @@ extends Node
 @export var misionId = 0
 @export var levelArray=["res://scenes/levels/level_1_0.tscn",
 "res://scenes/levels/level_1_1.tscn",
-"res://scenes/levels/level_1_2.tscn"]
+"res://scenes/levels/level_1_2.tscn",
+"res://scenes/levels/level_1_3.tscn",
+"res://scenes/levels/level_1_4.tscn"]
 
 
 
