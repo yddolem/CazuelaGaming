3a6e13f7a8703e0caab5e4694491a1fd661e58e7
Zelcris <xzelcris@gmail.com>
2023-07-20 04:12:12 -0400

    Last level added

--- cazuela gaming/current_scene.gd
+++ cazuela gaming/current_scene.gd
@@ -4,7 +4,8 @@ extends Node
 "res://scenes/levels/level_1_1.tscn",
 "res://scenes/levels/level_1_2.tscn",
 "res://scenes/levels/level_1_4.tscn",
-"res://scenes/levels/level_1_3.tscn"]
+"res://scenes/levels/level_1_3.tscn",
+"res://scenes/levels/level_1_5.tscn"]
 
 
 
