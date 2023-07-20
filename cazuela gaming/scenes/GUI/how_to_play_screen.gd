79069c186389d1bca95ece64545cebbd18cd0ae8
Sebamon2040 <sebastianmonteirop@gmail.com>
2023-07-20 12:23:40 -0400

    fixed credits bug

--- cazuela gaming/scenes/GUI/how_to_play_screen.gd
+++ cazuela gaming/scenes/GUI/how_to_play_screen.gd
@@ -1,14 +1,14 @@
 extends Control
 
-
+@onready var ost = $OSTPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	ost.play(Ostplayer.guiSongPosition)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	Ostplayer.guiSongPosition =ost.get_playback_position()
 
 
 func _on_main_menu_button_pressed():
--- cazuela gaming/scenes/GUI/main_menu.gd
+++ cazuela gaming/scenes/GUI/main_menu.gd
@@ -1,10 +1,12 @@
 extends Control
 var currentLevel
-
+@onready var ost = $OSTPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	ost.play(Ostplayer.guiSongPosition)
 	
+func _physics_process(delta):
+	Ostplayer.guiSongPosition = ost.get_playback_position()
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1_0.tscn")
