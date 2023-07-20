07914d88866aaa61c572e7b13a7b3c3c9b8e3c80
Zelcris <xzelcris@gmail.com>
2023-07-20 03:17:27 -0400

    Order changed

--- cazuela gaming/current_scene.gd
+++ cazuela gaming/current_scene.gd
@@ -3,8 +3,8 @@ extends Node
 @export var levelArray=["res://scenes/levels/level_1_0.tscn",
 "res://scenes/levels/level_1_1.tscn",
 "res://scenes/levels/level_1_2.tscn",
-"res://scenes/levels/level_1_3.tscn",
-"res://scenes/levels/level_1_4.tscn"]
+"res://scenes/levels/level_1_4.tscn",
+"res://scenes/levels/level_1_3.tscn"]
 
 
 
